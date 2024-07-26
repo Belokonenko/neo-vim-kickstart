6aef9f2efca5d9a05551e20f0e92c4adba10b02a
Belokonenko <Belokonenko.Viacheslav@gmail.com>
2024-07-26 17:21:47 +0300

    save

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 document.ddEventListener('DOMContentLoaded', () => {
-  function test2() {
-    console.log();
-  }
-  array.forEach((element) => {});
+    function test2() {
+        console.log();
+    }
+    array.forEach((element) => {});
 });
